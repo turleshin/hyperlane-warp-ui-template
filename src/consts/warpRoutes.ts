a1abfa5318eaed73b3bbf5a165561d98525ed270
shin <74643858+turleshin@users.noreply.github.com>
2024-08-05 13:56:29 +0700

    Update warpRoutes.ts

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -6,23 +6,23 @@ import { TokenStandard } from '@hyperlane-xyz/sdk';
 export const warpRouteConfigs: WarpCoreConfig = {
   tokens: [
     {
-		chainName: arbitrumsepolia,
+		chainName: 'arbitrumsepolia',
 		standard: TokenStandard.EvmHypCollateral,
 		decimals: 6,
 		logoURI: '/deployments/warp_routes/USDC/logo.svg',
-		symbol: USDC,
-		name: USD Coin,
+		symbol: 'USDC',
+		name: 'USD Coin',
 		addressOrDenom: "0x31861e286A4C5d29d68f4D514B7421109cb48FA1",
 		collateralAddressOrDenom: "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d",
 		connections: [ { token: 'ethereum|basesepolia|0x73aB51830d853739b8aC044235FC470f681762bD' } ]
 	},
 	{
-		chainName: basesepolia,
-		standard: EvmHypSynthetic,
+		chainName: 'basesepolia',
+		standard: TokenStandard.EvmHypSynthetic,
 		decimals: 6,
 		logoURI: '/deployments/warp_routes/USDC/logo.svg',
-		symbol: USDC,
-		name: USD Coin,
+		symbol: 'USDC',
+		name: 'USD Coin',
 		addressOrDenom: "0x73aB51830d853739b8aC044235FC470f681762bD",
 		connections: [ { token: 'ethereum|arbitrumsepolia|0x31861e286A4C5d29d68f4D514B7421109cb48FA1' } ]
 	}
