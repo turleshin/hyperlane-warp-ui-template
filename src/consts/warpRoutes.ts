e5af63ad0d09351cba85673a2fdf7cd9166d1f39
shin <74643858+turleshin@users.noreply.github.com>
2024-08-05 13:50:42 +0700

    Update warpRoutes.ts

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -1,9 +1,32 @@
 import { WarpCoreConfig } from '@hyperlane-xyz/sdk';
-
+import { TokenStandard } from '@hyperlane-xyz/sdk';
 // A list of Warp Route token configs
 // These configs will be merged with the warp routes in the configured registry
 // The input here is typically the output of the Hyperlane CLI warp deploy command
 export const warpRouteConfigs: WarpCoreConfig = {
-  tokens: [],
+  tokens: [
+    {
+		chainName: arbitrumsepolia,
+		standard: TokenStandard.EvmHypCollateral,
+		decimals: 6,
+		logoURI: '/deployments/warp_routes/USDC/logo.svg',
+		symbol: USDC,
+		name: USD Coin,
+		addressOrDenom: "0x31861e286A4C5d29d68f4D514B7421109cb48FA1",
+		collateralAddressOrDenom: "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d",
+		connections: [ { token: 'ethereum|basesepolia|0x73aB51830d853739b8aC044235FC470f681762bD' } ]
+	},
+	{
+		chainName: basesepolia,
+		standard: EvmHypSynthetic,
+		decimals: 6,
+		logoURI: '/deployments/warp_routes/USDC/logo.svg',
+		symbol: USDC,
+		name: USD Coin,
+		addressOrDenom: "0x73aB51830d853739b8aC044235FC470f681762bD",
+		connections: [ { token: 'ethereum|arbitrumsepolia|0x31861e286A4C5d29d68f4D514B7421109cb48FA1' } ]
+	}
+  ],
   options: {},
 };
+
