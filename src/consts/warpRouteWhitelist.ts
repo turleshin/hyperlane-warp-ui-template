e5556fb74570d88ccb0a436bf9ff7b543e9eb53f
shin <74643858+turleshin@users.noreply.github.com>
2024-08-05 11:38:06 +0700

    Update warpRouteWhitelist.ts

--- src/consts/warpRouteWhitelist.ts
+++ src/consts/warpRouteWhitelist.ts
@@ -2,8 +2,6 @@
 // Warp Route IDs use format `SYMBOL/chainname1-chainname2...` where chains are ordered alphabetically
 // If left null, all warp routes in the configured registry will be included
 // If set to a list (including an empty list), only the specified routes will be included
-export const warpRouteWhitelist: Array<string> | null = null;
-// Example:
-[
+export const warpRouteWhitelist: Array<string> = [
   'USDC/arbitrumsepolia-basesepolia'
 ];
