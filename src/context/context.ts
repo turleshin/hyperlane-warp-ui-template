78272c1bfcc2226cf64d4a883c2372b8f966a21d
shin <74643858+turleshin@users.noreply.github.com>
2024-08-05 11:19:22 +0700

    Update context.ts

--- src/context/context.ts
+++ src/context/context.ts
@@ -33,6 +33,7 @@ export async function initWarpContext() {
   const coreConfig = await assembleWarpCoreConfig();
   const warpCore = WarpCore.FromConfig(multiProvider, coreConfig);
   warpContext = { registry, chains, multiProvider, warpCore };
+  console.log(warpContext);
   return warpContext;
 }
 
