cf94110a9689aec358b2563e9816abc65adcb932
shin <74643858+turleshin@users.noreply.github.com>
2024-08-05 12:19:15 +0700

    Update app.ts

--- src/consts/app.ts
+++ src/consts/app.ts
@@ -1,4 +1,4 @@
 export const APP_NAME = 'Hyperlane x Movement Brigde';
 export const APP_DESCRIPTION = 'A DApp for Hyperlane Warp Route transfers';
-export const APP_URL = 'hyperlane-warp-template.vercel.app';
+export const APP_URL = 'https://hyperlane-movement.vercel.app/';
 export const APP_BRAND_COLOR = '#025aa1';
